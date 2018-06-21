fac9e7e85341e49019654f51a1caaa9cff858eca
Kye Morton <pryre.dev@outlook.com>
2018-06-21 13:35:28 +1000

    Fixed up some bad and old logic

--- include/pid_controller_lib/pidController.h
+++ include/pid_controller_lib/pidController.h
@@ -16,21 +16,20 @@ class pidController {
 		double ki_;
 		double kd_;
 
-		double tau_;
 		double output_max_;
 		double output_min_;
 
 		double x_;				//Current state
-		double x_dot_;			//Current derivative state
 		double sp_;				//Curret setpoint
 		double control_output_;	//Latest control output
 
 		double integrator_;
-		double x_prev_;
+		double x_prev_;			//Previous state
+		double e_prev_;			//Previous calculated error
 
 	public:
 		pidController(ros::NodeHandle *nhp, std::string name);
-		pidController(ros::NodeHandle *nhp, std::string name, double initial_x, double initial_x_dot, double initial_setpoint, double initial_output );
+		pidController(ros::NodeHandle *nhp, std::string name, double initial_x, double initial_setpoint, double initial_output );
 		~pidController();
 
 		void reset();
@@ -39,17 +38,14 @@ class pidController {
 		void setKp( double Kp );
 		void setKi( double Ki );
 		void setKd( double Kd );
-		void setTau( double tau );
 		void setGains( double Kp, double Ki, double Kd );
 		bool setOutputMinMax( double min, double max );
 
 		double step( double dt, double sp, double x );
-		double step( double dt, double sp, double x, double x_dot );
 
 		double getKp();
 		double getKi();
 		double getKd();
-		double getTau();
 		double getOutputMin();
 		double getOutputMax();
 		double getOutput();
--- src/pid_controller_lib/pidController.cpp
+++ src/pid_controller_lib/pidController.cpp
@@ -14,36 +14,31 @@ pidController::pidController(ros::NodeHandle *nhp, std::string name) :
 	kp_( 0.0 ),
 	ki_( 0.0 ),
 	kd_( 0.0 ),
-	tau_( 0.0 ),
+	output_min_( 0.0 ),
+	output_max_( 0.0 ),
 	x_( 0.0 ),
-	x_dot_( 0.0 ),
 	sp_( 0.0 ),
 	control_output_( 0.0 ) {
 
 	dyncfg_control_params_.setCallback(boost::bind(&pidController::callback_cfg_params, this, _1, _2));
-
-	setOutputMinMax( -1.0, 1.0 );
-
-	this->reset();
+	reset();
 }
 
-pidController::pidController(ros::NodeHandle *nhp, std::string name, double initial_x, double initial_x_dot, double initial_setpoint, double initial_output ) :
+pidController::pidController(ros::NodeHandle *nhp, std::string name, double initial_x, double initial_setpoint, double initial_output ) :
 	nhp_(nhp),
-	dyncfg_control_params_(ros::NodeHandle(*nhp, name + "/params")),
+	dyncfg_control_params_(ros::NodeHandle(*nhp, name)),
 	kp_( 0.0 ),
 	ki_( 0.0 ),
 	kd_( 0.0 ),
-	tau_( 0.0 ),
+	output_min_( 0.0 ),
+	output_max_( 0.0 ),
 	x_( initial_x ),
-	x_dot_( initial_x_dot ),
 	sp_( initial_setpoint ),
 	control_output_( initial_output ) {
 
 	dyncfg_control_params_.setCallback(boost::bind(&pidController::callback_cfg_params, this, _1, _2));
 
-	setOutputMinMax( -1.0, 1.0 );
-
-	this->reset();
+	reset( x_ );
 }
 
 pidController::~pidController() {
@@ -51,12 +46,13 @@ pidController::~pidController() {
 }
 
 void pidController::reset() {
-	this->reset( x_ );
+	reset( x_ );
 }
 
 void pidController::reset( double x_prev ) {
 	integrator_ = 0.0;
 	x_prev_ = x_prev;
+	e_prev_ = 0.0;
 }
 
 void pidController::setKp( double Kp ) {
@@ -79,14 +75,10 @@ void pidController::setKd( double Kd ) {
 	kd_ = Kd;
 }
 
-void pidController::setTau( double tau ) {
-	tau_ = tau;
-}
-
 void pidController::setGains( double Kp, double Ki, double Kd ) {
-	this->setKp( Kp );
-	this->setKi( Ki );
-	this->setKd( Kd );
+	setKp( Kp );
+	setKi( Ki );
+	setKd( Kd );
 }
 
 bool pidController::setOutputMinMax( double min, double max ) {
@@ -102,34 +94,11 @@ bool pidController::setOutputMinMax( double min, double max ) {
 	return success;
 }
 
-//Calculate x_dot
-double pidController::step( double dt, double sp, double x ) {
+//Calculate control step
+double pidController::step( double dt, double sp, double x) {
 	//Check to make sure the controller hasn't gone stale
 	if( dt > 1.0 ) {
-		this->reset( x );
-		dt = 0.0;
-	}
-
-	//Calculate D term (use dirty derivative if we don't have access to a measurement of the derivative)
-	//The dirty derivative is a sort of low-pass filtered version of the derivative.
-	double x_dot = ( ( 2.0 * tau_ - dt ) / ( 2.0 * tau_ + dt ) * x_dot_ ) + ( 2.0 / ( 2.0 * tau_ + dt ) * ( x - x_prev_ ) );
-
-	double output = this->step( dt, sp, x, x_dot );
-
-	//Save last state
-	x_prev_ = x;
-
-	return output;
-}
-
-//Calculate control step with known x_dot
-//	time_now: Current time in seconds
-double pidController::step( double dt, double sp, double x, double x_dot ) {
-	x_dot_ = x_dot;
-
-	//Check to make sure the controller hasn't gone stale
-	if( dt > 1.0 ) {
-		this->reset( x );
+		reset( x );
 		dt = 0.0;
 	}
 
@@ -146,7 +115,7 @@ double pidController::step( double dt, double sp, double x, double x_dot ) {
 
 	//If it is a stale controller, just skip this section
 	if( dt > 0.0 ) {
-		d_term = kd_ * x_dot_;
+		d_term = kd_ * ( (error - e_prev_) / dt );
 
 		//If the integrator is enabled
 		if(ki_ != 0.0) {
@@ -159,18 +128,23 @@ double pidController::step( double dt, double sp, double x, double x_dot ) {
 	}
 
 	//Sum three terms: u = p_term + i_term - d_term
-	double u = p_term + i_term - d_term;
+	double u = p_term - d_term;
+	double ui = u + i_term;
 
 	//Output Saturation
-	double u_sat = ( u > output_max_ ) ? output_max_ : ( (u < output_min_ ) ? output_min_ : u );
+	double u_sat = ( ui > output_max_ ) ? output_max_ : ( (ui < output_min_ ) ? output_min_ : ui );
 
 	//Integrator anti-windup
 	//If the pid controller has saturated and if the integrator is the cause
-	if( ( u != u_sat ) && ( fabs( i_term ) > fabs( u - p_term - d_term ) ) )
-			integrator_ = ( u_sat - p_term - d_term ) / ki_;	//Trim the integrator to what it should currently be to only just hit the maximum
+	if( ( ui != u_sat ) && ( fabs( i_term ) > fabs( u_sat - u ) ) )
+			integrator_ = ( u_sat - u ) / ki_;	//Trim the integrator to what it should currently be to only just hit the maximum
 
 	//Set output
 	control_output_ = u_sat;
+
+	//Save last state
+	x_prev_ = x;
+	e_prev_ = error;
 
 	return control_output_;
 }
@@ -187,10 +161,6 @@ double pidController::getKd() {
 	return kd_;
 }
 
-double pidController::getTau() {
-	return tau_;
-}
-
 double pidController::getOutputMin() {
 	return output_min_;
 }
@@ -204,11 +174,10 @@ double pidController::getOutput() {
 }
 
 void pidController::callback_cfg_params(pid_controller_lib::ControlParamsConfig &config, uint32_t level) {
-	this->setKp(config.p);
-	this->setKi(config.i);
-	this->setKd(config.d);
-	this->setTau(config.tau);
+	setKp(config.p);
+	setKi(config.i);
+	setKd(config.d);
 
-	if(!this->setOutputMinMax(config.min, config.max))
+	if(!setOutputMinMax(config.min, config.max))
 		ROS_ERROR("Invalid min/max config, ignoring");
 }
